44fc3da6d98a213fa34e85b3382757a9f5e82dcc
salo <salomontilla@outlook.com>
2025-05-17 23:07:17 -0500

    primeras funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,45 @@ const TURNS = {
   O: 'O'
 }
 
-const board = Array(9).fill(null)
 
 type Square = {
-  children: string
-  value: string
-  onClick: () => void
+  children?: string
+  value?: string
+  isSelected?: boolean
+  updateBoard?: (index:number) => void
+  index?: number
 }
 
-function Square({ children, value, onClick }: Square) {
+function Square({ children, isSelected, updateBoard, index }: Square) {
+  const classname = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    if (updateBoard && index !== undefined) {
+      updateBoard(index)
+    }
+  }
+
   return (
-    <button className='square' onClick={onClick}>
+    <div className={classname} onClick={handleClick}>
       {children}
-    </button>
+    </div>
   )
 }
-function App() {
 
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+  
+  function updateBoard(index: number) {
+    if (board[index]) return
+    const newBoard = [...board]
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    newBoard[index] = turn
+    setBoard(newBoard)
+    setTurn(newTurn)
+  }
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
@@ -31,14 +53,23 @@ function App() {
             return (
               <Square
                 key={index}
+                index={index}
                 value={board[index]}
-                onClick={() => {}}
+                updateBoard={updateBoard}
               >
                 {board[index]}
               </Square>
             )
           })
         }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
